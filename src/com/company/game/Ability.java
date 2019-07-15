b3b1e1c18d33f5a55afc86dcfd5076fff56886a6
AzamatAsanbekov <azamat.asanbekov18@gmail.com>
2019-07-15 07:27:23 +0600

    Initial commit

---
+++ src/com/company/game/Ability.java
@@ -0,0 +1,10 @@
+package com.company.game;
+
+public enum Ability {//сложный тип данных в которых есть перечисление
+    CRITICAL_DAMAGE,
+    SAVE_DAMAGE_AND_REVERT,
+    HEAL,
+    BOOST,
+    THUNDER_HIT
+}
+
---
+++ src/com/company/players/HavingAbility.java
@@ -0,0 +1,9 @@
+package com.company.players;
+
+
+import com.company.boss.Boss;
+
+    public interface HavingAbility {
+        void useAbility(Hero[] heroes, Boss boss);
+    }
+
